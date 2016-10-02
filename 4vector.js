3cd07fe6094c69567d209ad7b74a9985ff665668
ChrisspyB <christopher.bradley@live.co.uk>
2016-10-02 06:27:17 +0100

    Work on 4vector particle collision sim

--- 4vector.js
+++ 4vector.js
@@ -1,3 +1,4 @@
+(function() {
 "use strict"
 
 var frame_length = 100; // ms
@@ -11,15 +12,15 @@ var width = 900,
 // Define the inital particles
 
 var p_data = [];
-p_data.push({x:0,y:0,v:0,m:0,m0:10});
+p_data.push({x:0,y:0,v:0.00,m:0,m0:10});
 p_data.push({x:0,y:1,v:0.25,m:0,m0:10});
-p_data.push({x:0,y:2,v:0.5,m:0,m0:10});
+p_data.push({x:0,y:2,v:0.50,m:0,m0:10});
 p_data.push({x:0,y:3,v:0.75,m:0,m0:10});
 p_data.push({x:0,y:4,v:0.99,m:0,m0:10});
 
 // Build the st diagram
 
-var st = new SpacetimeDiagram("#stdiagram",xmin,xmax,tmin,tmax,width/1,width/1,false,2.5,true);
+var st = new SpacetimeDiagram("#stdiagram",xmin,xmax,tmin,tmax,width/1,width/1,false,2.5,false,false,false);
 for (var i=0; i<p_data.length; i++){
 	st.addParticle(0,0,p_data[i].v,true,0);
 }
@@ -108,4 +109,67 @@ var run = function() {
 	},frame_length);
 };
 
-run();
+var mergeParticles = function(p1,p2) {
+
+	var p1 = Object.assign({}, p1);
+	var p2 = Object.assign({}, p2);
+	var p3 = {};
+
+	p3.mom = p2.mom+p1.mom;
+	p3.E = p2.E+p1.E;
+
+	console.log(p3.mom,p3.E);
+
+	p3.ti = (p1.xi-p2.xi)/(p2.v-p1.v);
+	p3.xi = p1.xi+p1.v*p3.ti;
+	p3.m0 = Math.sqrt(p3.E*p3.E - p3.mom*p3.mom);
+
+	var a = (p3.E*p3.E-p3.m0*p3.m0)/(p3.m0*p3.m0);
+	p3.v = Math.sqrt(a/(a+1)) * (Math.sign(p3.mom));
+
+	p3.lt = Infinity
+	return p3;
+};
+
+var foo = function(e,p) {
+	var m = Math.sqrt(e*e-p*p);
+	var a = (e*e-m*m)/(m*m)
+	var v = Math.sqrt(a/(a+1));
+	console.log("m:"+m+" spd:"+v);
+};
+
+var p1 = {xi:-5,v:0.40,m0:3};
+var p2 = {v:-0.6,m0:90};
+
+p1.mom = p1.m0*p1.v/Math.sqrt(1-p1.v*p1.v);
+p1.E = Math.sqrt(p1.mom*p1.mom+p1.m0*p1.m0);
+p2.mom = p2.m0*p2.v/Math.sqrt(1-p2.v*p2.v);
+p2.E = Math.sqrt(p2.mom*p2.mom+p2.m0*p2.m0);
+
+p2.xi = p1.xi*p2.v/p1.v; // to ensure collision at x=0
+p1.lt = (p1.xi-p2.xi)/(p2.v-p1.v);
+p2.lt = p1.lt
+
+var p3 = mergeParticles(p1,p2);
+
+// offset so that collision occurs at t=0
+
+p1.ti=-p1.lt;
+p2.ti=-p1.lt;
+p3.ti-=p1.lt;
+
+
+var p2_data = [];
+p2_data.push(p1);
+p2_data.push(p2);
+p2_data.push(p3);
+
+var st2 = new SpacetimeDiagram("#stdiagram2",-10,10,-10,10,width/1,width/1,false,2,false,true,true);
+for (var i=0; i<p2_data.length; i++){
+	var p = p2_data[i];
+	st2.addParticle(p.xi,p.ti,p.v,false,p.lt,"","red");
+}
+
+st2.changeTime(-10);
+
+})();	
--- testgrounds.js
+++ testgrounds.js
@@ -2,7 +2,7 @@
 // SpacetimeDiagram (div,xmin,xmax,ymin,ymax,w,h,newtonian)
 var width = 900;
 
-var foo = new SpacetimeDiagram("#testgrounds",-0,20,-0,20,width/1,width/1,false,2.5,true,true,true);
+var foo = new SpacetimeDiagram("#testgrounds",-20,20,-20,20,width,width,false,1,true,true,true);
 foo.addParticle(0,0,0,true,0);
 foo.addParticle(0,0,0.25,true,0);
 foo.addParticle(0,0,1,false,20);
